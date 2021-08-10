644ae8cde8063125dda7c20b47284c0c1f3c88fe
Pulukuri Mohan Sai Chandra <mohansaichandrap@gmail.com>
2021-08-10 12:51:42 +0530

    Updated files

--- LinkedList/src/App.java
+++ LinkedList/src/App.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
@@ -5,18 +6,85 @@ public class App {
          
         Scanner input=new Scanner(System.in);
         LinkedList uselist=new LinkedList();
-        uselist.AddLast(20);
-        uselist.AddLast(30);
-        uselist.AddLast(40);
-        uselist.AddFirst(10); 
-        uselist.AddFirst(50);
-        uselist.AddFirst(100);
-        uselist.PrintTheList();
-        System.out.println();
-        System.out.println("The index of the element 100 is "+uselist.IndexOf(100));
-        System.out.println("Please enter the node position  and the item ");
-        uselist.InsertAt(input.nextInt(), input.nextInt());
-        uselist.PrintTheList();
+        System.out.println("--------------------------");
+        System.out.println("Operations on Linked List ");
+        System.out.println("--------------------------");
+        char userChoice;
+        do {
+            System.out.println("------------------------------------------------------------");
+            System.out.println("Below are the options for the operations on the linked list ");
+            System.out.println("1. Add at First");
+            System.out.println("2. Add at Last ");
+            System.out.println("3. Insert the data at particular node");
+            System.out.println("4. Remove the element at First");
+            System.out.println("5. Remove the element at Last");
+            System.out.println("6. Print the list ");
+            System.out.println("7. Size of the linkedList");
+            System.out.println("8. Convert the linked list  into Array");
+            
+            System.out.println("------------------------------------------------------------");
+                System.out.println();
+            System.out.println("Please choose an option ");
+             switch (input.nextInt()) {
+                 case 1:
+                 {
+                     System.out.println("Enter the number to add first ");
+                     uselist.AddFirst(input.nextInt());
+
+                 } break;
+                 case 2:
+                 {
+                    System.out.println("Enter the number to add Last ");
+                    uselist.AddLast(input.nextInt());
+
+                 } break;
+                 case 3:
+                 {
+                     System.out.println("Check the list to choose a data to change");
+                     uselist.PrintTheList();
+                     System.out.println("Enter the node position and data position" );
+                     uselist.InsertAt(input.nextInt(),input.nextInt());
+                     System.out.println("List after modification ")
+                     ;
+                     uselist.PrintTheList();
+
+                 } break;
+                 case 4:
+                 {
+                     uselist.RemoveFirst();
+
+                 } break;
+                 case 5:
+                 {
+
+                    uselist.RemoveLast();
+
+                 } break;
+                 case 6:
+                 {
+
+                    uselist.PrintTheList();
+                 } break;
+                 case 7:
+                 {
+                    System.out.println("The size of the linked list is ");
+                    System.out.println(uselist.Size());
+
+                 }break;
+                 case 8:
+                 {
+                    var array= uselist.toArray();
+                     System.out.println(Arrays.toString(array));
+
+                 }break;
+
+             
+                 
+             }
+
+             System.out.println("Do you wish to countinue the operations on the list(y/n)");
+              userChoice=input.next().charAt(0);
+        } while (userChoice=='y');
 
     }
 }
--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,9 +1,11 @@
 import java.sql.Date;
+import java.util.spi.CurrencyNameProvider;
 
 public class LinkedList {
     
     private Node first;
     private Node last;
+    private int size=0;
     private class Node
     {
         private int data;
@@ -27,6 +29,8 @@ public class LinkedList {
             last.next=node;
              last=node;
         }
+
+        size++;
     }
 
     public void AddFirst(int item)
@@ -43,6 +47,7 @@ public class LinkedList {
             node.next=first;
             first=node;
         }
+        size++;
 
     }
 
@@ -97,7 +102,71 @@ public class LinkedList {
 
         }
 
+       size++;
+       
+     }
+
+     public boolean Contains(int item)
+     {
+         Node current=first;
+         boolean result=false;
+         while(current!=null)
+         {
+             if(current.data==item)
+              result=true;
+            
+
+            current=current.next;
+
+         }
+         return result;
+     }
+
+     public void RemoveFirst()
+     {
+
+         
+        first=first.next;
+        size--;
+     }
+
+     public void RemoveLast()
+     {
+         var current = first;
+         while(current!=null)
+         {
+             if(current.next.next==null)
+             {
+                 current.next=null;
+                 break;
+             }
+             
+             else{
+                 current=current.next;
+             }
+             size--;
+         }
+     }
+
+     public int Size()
+     {
+       
+        return size;
+     }
 
 
+     public int[] toArray()
+     {
+         int array[]=new int[size];
+         var current=first;
+         var index=0;
+         while(current!=null)
+         {
+             array[index]=current.data;
+             index++;
+             current=current.next;
+
+         }
+         return array;
      }
 }
